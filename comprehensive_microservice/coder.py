019af042f48d1a124ba7e604179e51a0c5039352
Dmitry Savinskikh <d.savinskikh@gmail.com>
2023-08-10 19:09:54 +0300

    implement business logic

--- comprehensive_microservice/coder.py
+++ comprehensive_microservice/coder.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from random import randint
 
 
 class DataModel(BaseModel):
@@ -7,8 +8,24 @@ class DataModel(BaseModel):
 
 
 class DataCoder:
+    def __init__(self):
+        self.storage = {}
+
     def save(self, data: DataModel) -> str:
-        raise NotImplementedError
+        while True:
+            new_id = str(randint(1, 100000))
+            if new_id not in self.storage.keys():
+                break
+        self.storage[new_id] = (data.title, data.text)
+        return new_id
 
     def get(self, code: str) -> DataModel:
-        raise NotImplementedError
+        tuple_ = self.storage[code]
+        data = DataModel(title=tuple_[0], text=tuple_[1])
+        return data
+
+
+# some_var = DataCoder()
+# id = some_var.save(DataModel(title="gavno", text="shit"))
+# print(id)
+# print(some_var.get(id))
