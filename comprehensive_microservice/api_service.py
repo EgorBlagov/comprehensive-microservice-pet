9ce7ea4e9d40458342826e9bf513a34e849d84c7
Dmitry Savinskikh <d.savinskikh@gmail.com>
2023-08-27 15:23:34 +0300

    write tests for api service and changed /get in api service

--- comprehensive_microservice/api_service.py
+++ comprehensive_microservice/api_service.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from .coder import DataCoder, DataModel, Storage
 
@@ -7,10 +7,14 @@ data_coder = DataCoder(Storage())
 
 
 @app.post("/save")
-async def save_data(data: DataModel) -> str:
-    return data_coder.save(data)
+async def save_data(data: DataModel) -> dict[str, str]:
+    key = data_coder.save(data)
+    return {"key": key}
 
 
-@app.get("/get")
-async def get_data(key: str) -> DataModel:
-    return data_coder.get(key)
+@app.get("/get/{item_id}")
+async def get_data(item_id: str) -> DataModel:
+    try:
+        return data_coder.get(item_id)
+    except KeyError:
+        raise HTTPException(status_code=404, detail="Path not found")
